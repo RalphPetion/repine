bd7e2d491f84665161caca023ae17fe77b5cb8be
Ralph Petion <ralphpetion@Ralphs-MacBook-Air.local>
2026-05-22 21:18:48 -0400

    inital commit

---
+++ backend/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+#instance of fastapi
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers=["*"],
+)
+
+#testing if back end is connected
+@app.get("/test")
+def test():
+    return{"message": "Backend successfully connected"}
